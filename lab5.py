487052f6a5f9370f29232fa07434d14f51509b40
void2177 <clb2377@gmail.com>
2025-09-30 12:05:16 -0700

    Saved progress; working on pt. 2

--- lab5.py
+++ lab5.py
@@ -24,3 +24,40 @@ def is_valid_ip(ip):
           is_valid_part(i)
 
 ## is_valid_ip("01.168.127.12")
+
+def decimal_to_binary(n):
+    if n == 0:
+        return "0"
+    elif n == 1:
+        return "1"
+    else:
+        return decimal_to_binary(n // 2) + str(n % 2)
+
+##print(decimal_to_binary(15))
+
+def binary_to_decimal(b):
+    if b == 0:
+        return "0"
+    else:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
